ab41b7cbf8b4a14e6a1529ffab1a72f20c337c7a
owenshi2 <owenshi2@illinois.edu>
2024-07-27 21:34:55 -0500

    Graph 1 Complete

--- clean-data.py
+++ clean-data.py
@@ -3,7 +3,7 @@ import numpy as np
 
 data = pd.read_csv('./data/student-data.csv')
 GPdata = data[(data['address'] == 'U')]
-GPdataAve = GPdata.filter(items=["school", "sex", "age", "studytime", "failures"])
+GPdataAve = GPdata.filter(items=["school", "sex", "age", "studytime", "failures", "schoolsup"])
 GPdataAve["averageGrade"] = round(GPdata[['G1', 'G2', 'G3']].mean(axis=1) / 20 * 100, 2)
-GPdataAve["alcoholism1to5"] = round(GPdata[['Dalc', 'Walc']].mean(axis=1)).astype(int)
+GPdataAve["alcoholism1to5"] = GPdata[['Dalc', 'Walc']].mean(axis=1)
 GPdataAve.to_csv('./data/student-data-clean-noindex.csv', encoding='utf-8', index=False)
--- script.js
+++ script.js
@@ -61,7 +61,7 @@ function addParagraphForScene(sceneIndex) {
     let paragraphContent;
     switch (sceneIndex) {
         case 0:
-            paragraphContent = "Scene1.";
+            paragraphContent = "Scene1. Now, let's see the correlation on studying and grades by hitting the \"Next\" button up top...";
             break;
         case 1:
             paragraphContent = "Scene2.";
@@ -82,92 +82,172 @@ function addParagraphForScene(sceneIndex) {
 
 async function drawScatter(dataInp)
 {
-    const data = dataInp
-    console.log(data)
-    const xAccess = (d) => d.alcoholism1to5;
-    const yAccess = (d) => d.averageGrade;
-    const colorAccess = (d) => d.school;
-    const width = d3.min([window.innerWidth * 0.9, window.innerHeight * 0.9]);
-    const dimensions = {
-        width,
-        height: width,
-        margins: {
-        top: 10,
-        right: 10,
-        bottom: 50,
-        left: 50
-        }
-    };
-    dimensions.boundedWidth =
-    dimensions.width - dimensions.margins.left - dimensions.margins.right;
-    dimensions.boundedHeight =
-    dimensions.height - dimensions.margins.top - dimensions.margins.bottom;
+    var studTime = d => d["studytime"];
+    var schoolTip = d => d["school"];
+    const margin = {top: 20, right: 30, bottom: 30, left: 40},
+          width = +d3.select("svg").attr("width") - margin.left - margin.right,
+          height = +d3.select("svg").attr("height") - margin.top - margin.bottom;
 
-  const wrapper = d3
-    .select("#wrapper")
-    .append("svg")
-    .attr("width", dimensions.width)
-    .attr("height", dimensions.height);
+    const x = d3.scaleLinear().domain([1, d3.max(dataInp, d => d.alcoholism1to5)]).range([0, width]),
+          y = d3.scaleLinear().domain([0, 100]).range([height, 0]),
+          color = d3.scaleOrdinal().domain(d3.extent(dataInp, d => d.school)).range(["orangered", "blue"]);
+          //     .scaleLinear()
+          //     .domain(d3.extent(data, colorAccess))
+          //     .range(["skyblue", "darkslategray"]);
 
-  const bounds = wrapper
-    .append("g")
-    .style(
-      "transform",
-      `translate(${dimensions.margins.left}px, ${dimensions.margins.top}px)`
-    );
-
-    // Createing Scales
-    const xScale = d3
-        .scaleLinear()
-        .domain(d3.extent(data, xAccess))
-        .range([0, dimensions.boundedWidth])
-        .nice();
-
-    const yScale = d3
-        .scaleLinear()
-        .domain(d3.extent(data, yAccess))
-        .range([dimensions.boundedHeight, 0])
-        .nice();
-
-    const colorScale = d3
-        .scaleLinear()
-        .domain(d3.extent(data, colorAccess))
-        .range(["skyblue", "darkslategray"]);
-
-    //  Draw data
-    const dots = bounds
+    const g = d3.select('svg')
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    
+    // data plot
+    d3.select('svg')
         .selectAll("circle")
-        .data(data)
+        .data(dataInp)
         .enter()
         .append("circle")
-        .attr("cx", (d) => xScale(xAccess(d)))
-        .attr("cy", (d) => yScale(yAccess(d)))
+        .attr("cx", (d) => x(d.alcoholism1to5) + margin.top + 30)
+        .attr("cy", (d) => y(d.averageGrade) + margin.left)
+        .attr("fill", (d) => color(d.school))
         .attr("r", 4)
-        .attr("fill", (d) => colorScale(colorAccess(d)));
-    const xAxisGenerator = d3.axisBottom().scale(xScale);
+        //Mouseover Events
+        .on('mouseover', function (d, i) {
+            d3.select(this).transition()
+                .duration('100')
+                .attr("r", 7);
+            //Makes div appear
+            div.transition()
+                .duration('200')
+                .style("opacity", 1);
+            //Uses data
+            var yPos = d3.select(this).attr("cy");
+            var xPos = d3.select(this).attr("cx");
+            div.html(`Study Time: ${studTime(i)} <br />School: ${schoolTip(i)}`)
+                .style("top", (parseFloat(yPos) + 225) + "px")
+                .style("left", (parseFloat(xPos) + 500) + "px");
+        }).on('mouseout', function (d, i) {
+            d3.select(this).transition()
+                .duration('200')
+                .attr("r", 4);
+            //makes div disappear
+            div.transition()
+                .duration('200')
+                .style("opacity", 0);
+        });
 
-    const xAxis = bounds
-        .append("g")
-        .call(xAxisGenerator)
-        .style("transform", `translateY(${dimensions.boundedHeight}px)`);
-    const xAxisLabel = xAxis
-    .append("text")
-    .attr("x", dimensions.boundedWidth / 2)
-    .attr("y", dimensions.margins.bottom - 10)
-    .attr("fill", "black")
-    .style("font-size", "1.4em")
-    .html("Alcohol Consumption 1 (Low) to 5 (High)");
-    
-    const yAxisGenerator = d3.axisLeft().ticks(5).scale(yScale);
+    var div = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+   
 
-    const yAxis = bounds.append("g").call(yAxisGenerator);
-    const yAxisLabel = yAxis
-    .append("text")
-    .attr("x", -dimensions.boundedHeight / 2)
-    .attr("y", -dimensions.margins.left + 10)
-    .style("fill", "black")
-    .style("transform", "rotate(-90deg)")
-    .html("Average Grade")
-    .style("font-size", "1.4em")
-    .style("text-anchor", "middle");
+    // x axis
+    g.append("g")
+        .attr("class", "axis axis--x")
+        .attr("transform", "translate(" + (30 + margin.top) + "," + (height + margin.top) + ")")
+        .call(d3.axisBottom(x))
+        .append("text")
+        .attr("transform", "translate(-30, 0)")
+        .attr("fill", "black")
+        .attr("x", width - margin.right + 5)
+        .attr("y", -15)
+        .attr("dy", "0.71em")
+        .attr("text-anchor", "start")
+        .text("Alcohol Levels");
+    // y axis
+    g.append("g")
+        .attr("class", "axis axis--y")
+        .attr("transform", "translate("+ (30 + margin.top) + ","+ margin.top +")")
+        .call(d3.axisLeft(y))
+      .append("text")
+        .attr("fill", "black")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", "0.71em")
+        .attr("text-anchor", "end")
+        .text("Grades");
+
+    console.log(dataInp);
+    // g.append("path")
+    //     .datum(dataInp)
+    //     .attr("class", "line")
+    //     .attr("fill", "none")
+    //     .attr("stroke", "black")
+    //     .attr("stroke-width", 1.5)
+    //     .attr("d", line);
+    // const data = dataInp
+    // console.log(data)
+    // const xAccess = (d) => d.alcoholism1to5;
+    // const yAccess = (d) => d.averageGrade;
+    // const colorAccess = (d) => d.school;
+    // const margin = {
+    //     top: 20,
+    //     right: 30,
+    //     bottom: 30,
+    //     left: 40
+    // };
+    // const width = d3.select("svg").attr("width") - margin.left - margin.right;
+    // const height = d3.select("svg").attr("height") - margin.top - margin.bottom;
+    // const xScale = d3
+    //     .scaleLinear()
+    //     .domain(d3.extent(data, xAccess))
+    //     .range([0, width]);
+
+    // const yScale = d3
+    //     .scaleLinear()
+    //     .domain([0, d3.max(data, yAccess)])
+    //     .range([height, 0]);
+
+    // const colorScale = d3
+    //     .scaleLinear()
+    //     .domain(d3.extent(data, colorAccess))
+    //     .range(["skyblue", "darkslategray"]);
+
+    // const dots = d3.select('svg')
+    //     .selectAll("circle")
+    //     .data(data)
+    //     .enter()
+    //     .append("circle")
+    //     .attr("cx", (d) => xScale(xAccess(d)))
+    //     .attr("cy", (d) => yScale(yAccess(d)))
+    //     .attr("r", 4)
+    //     .attr("fill", (d) => colorScale(colorAccess(d)));
+
+    // console.log(data);
+
+
+    // const gSVG = d3.select('svg').attr("transform", "translate("+ margin.left +"," + margin.top +")");
+
+    // gSVG
+    //     .attr("class", "axis axis--x")
+    //     .attr("transform", "translate("+ 10 + "," + height + ")")
+    //     .call(d3.axisBottom(xScale))
+    // .append("text")
+    // .attr("fill", "black")
+    // .attr("x", width - margin.right + 5)
+    // .attr("y", -10)
+    // .style("font-size", "1.4em")
+    // .html("Alcohol Consumption 1 (Low) to 5 (High)");
+
+    // const yAxis = gSVG.append("g")
+    // .attr("class", "axis axis--y")
+    // .call(d3.axisLeft(yScale));
+    // const yAxisLabel = yAxis
+    // .append("text")
+    // .attr("fill", "black")
+    // .style("transform", "rotate(-90)")
+    // .attr("y", -margin.left + 50)
+    // .attr("dy", "0.71em")
+    // .attr("text-anchor", "end")
+    // .html("Average Grade");
+}
+
+// bar has study time with grades, with another variable (size?) detailing the drinking
+async function drawBar(dataInp)
+{
+    const margin = {top: 20, right: 30, bottom: 30, left: 40},
+          width = +d3.select("svg").attr("width") - margin.left - margin.right,
+          height = +d3.select("svg").attr("height") - margin.top - margin.bottom;
+
+    const x = d3.scaleLinear().domain([1, d3.max(dataInp, d => d.alcoholism1to5)]).range([0, width]),
+          y = d3.scaleLinear().domain([0, 100]).range([height, 0]),
+          color = d3.scaleOrdinal().domain(d3.extent(dataInp, d => d.school)).range(["orangered", "blue"]);
+          //
 }
